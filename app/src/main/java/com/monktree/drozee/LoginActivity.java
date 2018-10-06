3069881a896b9509b81b67a7266ddd0c4aa1fcdd
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2018-10-06 11:44:20 +0530

    Initial Commit for Drozee App

---
+++ app/src/main/java/com/monktree/drozee/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.monktree.drozee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
